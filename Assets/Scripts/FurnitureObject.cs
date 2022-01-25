aad8b0b3300ba931b46272081e93529f9534c9b6
Tom O'Leary <to231922@falmouth.ac.uk>
2022-01-25 14:15:25 +0000

    FurnitureObject sides array now applies enum values by default

--- Assets/Scripts/FurnitureObject.cs
+++ Assets/Scripts/FurnitureObject.cs
@@ -1,14 +1,28 @@
 using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Furniture", menuName = "Create New Furniture")]
 public class FurnitureObject : ScriptableObject
 {
+    static FurnitureObject()
+    {
+        Sides[] sides = new Sides[6];
+        foreach(var v in Enum.GetValues(typeof(AxisDirections)))
+        {
+            sides[(int)v].axis = (AxisDirections)v;
+            sides[(int)v].distance = 1.0f;
+        }
+
+        s_DefaultEnumValues = new ReadOnlyCollection<Sides>(sides);
+    }
+    static readonly ReadOnlyCollection<Sides> s_DefaultEnumValues;
 
     public string furnitureName;
 
     public AgentState state;
-    public Sides[] sides;
+    public Sides[] sides = s_DefaultEnumValues.ToArray();
 
     //public FurnitureObject[] potentialParents;
 
@@ -26,7 +40,7 @@ public struct Sides
     public float clearanceSpace;
 
     public int numberOfChildren;
-    
+
     public LayerMask layers;
 }
 
