efd998a0ed1a485b0e7b93e105c118dc94a0cc90
Tom O'Leary <to231922@falmouth.ac.uk>
2022-02-01 11:00:01 +0000

    Added commenting to FurnitureObject.cs

--- Assets/Scripts/FurnitureObject.cs
+++ Assets/Scripts/FurnitureObject.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Furniture", menuName = "Create New Furniture")]
 public class FurnitureObject : ScriptableObject
 {
+    
+    /// <summary>
+    /// Constructor that allocates a default side & distance to each element in the Sides array
+    /// </summary>
     static FurnitureObject()
     {
         Sides[] sides = new Sides[6];
@@ -15,20 +19,38 @@ public class FurnitureObject : ScriptableObject
             sides[(int)v].distance = 1.0f;
         }
 
-        defaultEnumValues = new ReadOnlyCollection<Sides>(sides);
+        defaultSidesValues = new ReadOnlyCollection<Sides>(sides);
     }
-    static readonly ReadOnlyCollection<Sides> defaultEnumValues;
+    static readonly ReadOnlyCollection<Sides> defaultSidesValues;
 
+    /// <summary>
+    /// The furnitures name
+    /// </summary>
     public string furnitureName;
 
+    /// <summary>
+    /// The Agents state
+    /// </summary>
     public AgentState state;
-    public Sides[] sides = defaultEnumValues.ToArray();
 
+    /// <summary>
+    /// The Agents sides.
+    /// </summary>
+    public Sides[] sides = defaultSidesValues.ToArray();
+
+    /// <summary>
+    /// An array of potential parents for the Agent
+    /// </summary>
     public Parent[] potentialParents;
 
+    /// <summary>
+    /// The current parent of the Agent
+    /// </summary>
     public FurnitureObject currentParent;
 
-
+    /// <summary>
+    /// Returns an instance of the Agent
+    /// </summary>
     public FurnitureObject GetInstance()
     {
         return Instantiate(this);
@@ -36,30 +58,53 @@ public class FurnitureObject : ScriptableObject
     
 }
 
+
+/// <summary>
+/// Sides struct that contains data required for each side of the Agent
+/// </summary>
 [Serializable]
 public struct Sides
 {
+    // The direction of the side
     public AxisDirections axis;
+
+    // The distance of the side
 
     [Range(0.0f, 10.0f)]
     public float distance;
 
+    // How much clearance space this distance requires
     public float clearanceSpace;
 
+    // How many children can be placed on the side
     public int spaceForChildren;
+
+    // How many current children are placed on the side
     public int currentChildren;
 
+    // The layer that the side can raycast
     public LayerMask layers;
 }
 
+/// <summary>
+/// Parent struct that handles neccessary data about a parent
+/// </summary>
 [Serializable]
 public struct Parent
 {
+    // The type of agent the parent is
     public FurnitureObject parent;
+    
+    // What side on the parent the agent should be placed on
     public AxisDirections sideOnParent;
+
+    // What side the agent should place on the parent
     public AxisDirections sideOnAgent;
 }
 
+/// <summary>
+/// All directions that are used
+/// </summary>
 public enum AxisDirections
 {
     UP,
@@ -70,6 +115,10 @@ public enum AxisDirections
     BACK
 }
 
+
+/// <summary>
+/// The Agents state
+/// </summary>
 public enum AgentState
 {
     SEARCH,
