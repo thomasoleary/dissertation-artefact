cae1fa3442d8843f4f059260c7d452379a63eaac
Tom O'Leary <to231922@falmouth.ac.uk>
2022-02-06 12:39:33 +0000

    Stupidly I'm deciding now to refactor the Agents structure...

--- Assets/Scripts/FurnitureObject.cs
+++ Assets/Scripts/FurnitureObject.cs
@@ -78,35 +78,7 @@ public class FurnitureObject : ScriptableObject
 }
 
 
-/// <summary>
-/// Sides struct that contains data required for each side of the Agent
-/// </summary>
-[Serializable]
-public struct Sides
-{
-    // The direction of the side
-    public AxisDirections axis;
 
-    // The distance of the side
-
-    [Range(0.0f, 10.0f)]
-    public float distance;
-
-    // How much clearance space this distance requires
-    public float clearanceSpace;
-
-    // How many children can be placed on the side
-    public int spaceForChildren;
-
-    // How many current children are placed on the side
-    public int currentChildren;
-
-    // If the side has max amount of children
-    public bool hasMaxChildren;
-
-    // The layer that the side can raycast
-    public LayerMask layers;
-}
 
 /// <summary>
 /// Parent struct that handles neccessary data about a parent
@@ -126,39 +98,9 @@ public struct Parent
     public AxisDirections sideOnAgent;
 }
 
-/// <summary>
-/// All directions that are used
-/// </summary>
-public enum AxisDirections
-{
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT,
-    FORWARD,
-    BACK
-}
 
 
-/// <summary>
-/// The Agents state
-/// </summary>
-public enum AgentState
-{
-    SEARCH,
-    ARRANGE,
-    REST,
-    SLEEP
-}
 
-public enum TypeOfFurniture
-{
-    BED,
-    BEDSIDE_CABINET,
-    CHAIR,
-    DESK,
-    DRAWERS,
-    FLOOR,
-    TABLE_LAMP,
-    WARDROBE
-}
+
+
+
