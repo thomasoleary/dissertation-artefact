6de2013115b0bbd415a9e2ce8a70a6b6c6e6999e
Tom O'Leary <to231922@falmouth.ac.uk>
2022-03-24 16:52:29 +0000

    Added commenting to RoomArrays and Spawner

--- Assets/Scripts/Research/RoomArrays.cs
+++ Assets/Scripts/Research/RoomArrays.cs
@@ -2,6 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* 
+    File: RoomArrays.cs
+    Author: Thomas (Tom) O'Leary
+
+    Summary:
+        This is a small class that is only used to contain what 
+        Human Rooms & Artefact rooms being used in the Research Study.
+ */
+
 public class RoomArrays : MonoBehaviour
 {
     public GameObject[] humanRooms;
--- Assets/Scripts/Research/Spawner.cs
+++ Assets/Scripts/Research/Spawner.cs
@@ -2,18 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+/* 
+    File: Spawner.cs
+    Author: Thomas (Tom) O'Leary
 
+    Summary:
+        This class is responsible for randomly selecting the order in which 
+        the Pair of rooms get shown during the Research Study.
+ */
 public class Spawner : MonoBehaviour
 {
-    public RoomArrays refArrays;
 
+    #region Variables
+    [Header("Arrays/Lists")]
+    public RoomArrays rooms;
     [SerializeField] private List<GameObject> hRooms;
     [SerializeField] private List<GameObject> aRooms;
 
+    
     [Header("Current Rooms")]
     public GameObject currentHumanRoom;
     public GameObject currentArtefactRoom;
-
     public int currentPairID = 0;
 
     GameObject spawnedHumanRoom;
@@ -22,15 +31,24 @@ public class Spawner : MonoBehaviour
     [Header("Spawns")]
     public Transform[] spawns;
 
+    [Header("Misc")]
     [HideInInspector] public bool isArtefactLeftSide = false;
-
     int randomIndex = 0;
 
+    #endregion Variables
+
+    ///<summary>
+    /// Creates a temporary list for the Rooms to be picked & deleted from
+    ///</summary>
     public void SetTempLists()
     {
-        hRooms = refArrays.humanRooms.ToList();
-        aRooms = refArrays.artefactRooms.ToList();
+        hRooms = rooms.humanRooms.ToList();
+        aRooms = rooms.artefactRooms.ToList();
     }
+
+    ///<summary>
+    /// Selects the next Pair of rooms
+    ///</summary>
     public void PickRoomPair()
     {
         randomIndex = Random.Range(0, hRooms.Count);
@@ -43,26 +61,34 @@ public class Spawner : MonoBehaviour
         aRooms.RemoveAt(randomIndex);
     }
 
+    ///<summary>
+    /// Spawns the pair of rooms on two Transforms in the scene.
+    ///</summary>
     public void SpawnPair()
     {
         PickRoomPair();
         // pick a number 0 or 1
         if (Random.value < 0.5f)
         {
-            // Human spawns left
+            // Human Spawns Left
             spawnedHumanRoom = Instantiate(currentHumanRoom, spawns[0].transform.position, spawns[0].transform.rotation);
+            // Artefact Spawns Right
             spawnedArtefactRoom = Instantiate(currentArtefactRoom, spawns[1].transform.position, spawns[1].transform.rotation);
             isArtefactLeftSide = false;
         }
         else
         {
-            // artefact spawns left
+            // Artefact Spawns Left
             spawnedArtefactRoom = Instantiate(currentArtefactRoom, spawns[0].transform.position, spawns[0].transform.rotation);
+            // Human Spawns Right
             spawnedHumanRoom = Instantiate(currentHumanRoom, spawns[1].transform.position, spawns[1].transform.rotation);
             isArtefactLeftSide = true;
         }
     }
 
+    ///<summary>
+    /// Despawns the current pair of Rooms
+    ///</summary>
     public void DespawnPair()
     {
         Destroy(spawnedHumanRoom);
@@ -70,11 +96,15 @@ public class Spawner : MonoBehaviour
         isArtefactLeftSide = false;
     }
 
+    ///<summary>
+    /// Get's the Room's pair ID.
+    /// For e.g. aRoom1 & hRoom1 is picked = the pair ID is 1
+    ///</summary>
     public int GetPairID()
     {
-        for (int i = 0; i < refArrays.humanRooms.Count(); i++)
+        for (int i = 0; i < rooms.humanRooms.Count(); i++)
         {
-            if (currentHumanRoom == refArrays.humanRooms[i])
+            if (currentHumanRoom == rooms.humanRooms[i])
             {
                 return i + 1;
             }
